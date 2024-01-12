65ab5edcc55866b286f2e0c2e1c541d7c9eff6a6
Yahyobek <155903617+dreamdev-MYU@users.noreply.github.com>
2024-01-12 09:52:51 +0500

    dars_3 sorting algorithms

---
+++ darslar/dars_3/exercise1.py
@@ -0,0 +1,30 @@
+# my_list = [7,8,4,1]
+# a=0
+# def func(s):
+#   for i in s:
+#     if a<i:
+#       a==i
+#       result = i           
+#     else:
+#          result=a
+#     continue
+#   return result
+# print(func(my_list))
+
+
+my_list = [7, 8, 4, 1]
+print(min(my_list))
+
+# def func(s):
+#     for i in s:
+#         if a < i:
+#             a = i
+#             result = i
+#         else:
+#             result = a
+#         continue
+#     return result
+
+# print(func(my_list))
+
+
